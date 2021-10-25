d25b88f941cf987bfea827c68e6373055e85bd0e
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2021-10-25 13:12:21 +0200

    lab 4 ex 9

---
+++ archive/lab4/4.9.py
@@ -0,0 +1,10 @@
+print('Podaj bok trojkata')
+n = int(input())
+outStr = ''
+for i in range(n+1):
+	for j in range(n-i):
+		outStr += ' '
+	for j in range(i):
+		outStr += 'X'
+	print(outStr)
+	outStr=''
