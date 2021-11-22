648435a5d36e88e92da76eaaa97d69e113993e54
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2021-11-22 12:31:38 +0100

    lab07zad05

---
+++ lab07/0705.py
@@ -0,0 +1,16 @@
+def pietro(arg='x'):
+	print(10*arg)
+	print(3 * arg + 4 * ' ' + 3 * arg)
+	print(3 * arg + 4 * ' ' + 3 * arg)
+	print(10*arg)
+
+def dach(arg='x'):
+	print(4*' '+2*arg)
+	print(3*' '+4*arg)
+	print(2*' '+6*arg)
+	print(1*' '+8*arg)
+	print(10*arg)
+
+dach('^')
+pietro('#')
+pietro('#')
