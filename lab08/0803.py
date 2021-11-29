526249479be8d0afda0045e6f4b0cfbda123da1d
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2021-11-29 12:33:08 +0100

    lab08zad03

---
+++ lab08/0803.py
@@ -0,0 +1,11 @@
+def tiles(n, k):
+	if n == 1:
+		return n * k
+	else:
+		return (2 * (n-1) + 1) * k + tiles(n - 1, k)
+
+print(tiles(1, 2))
+print(tiles(2, 2))
+print(tiles(3, 2))
+print(tiles(4, 2))
+print(tiles(5, 2))
